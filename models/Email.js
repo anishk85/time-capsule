ed4bc4feb5a2ce6e4173e3646dab9540f1f81fca
anishk85 <anishkumar96465213@gmail.com>
2025-02-23 09:08:33 +0530

    added folder segragation

--- models/Email.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const EmailSchema = new mongoose.Schema({
-    email: String,
-    date: String,
-    imageUrl: { type: String }
-});
-
-module.exports = mongoose.model('Email', EmailSchema);
--- models/video.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const videoSchema = new mongoose.Schema({
-    url: { type: String, required: true },
-    tags: { type: [String], default: [] },
-});
-
-module.exports = mongoose.model("Video", videoSchema);
